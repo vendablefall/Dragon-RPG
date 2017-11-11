a57efeedc7079ae42771452ded2d99b3c75d7b28
vendablefall <vendablefall@leet.cloud>
2017-11-11 15:45:47 +1000

    AI shiz

--- Assets/Camera & UI/CameraRaycaster.cs
+++ Assets/Camera & UI/CameraRaycaster.cs
@@ -51,6 +51,7 @@ public class CameraRaycaster : MonoBehaviour
         // Otherwise return background hit
         raycastHit.distance = distanceToBackground;
         layerHit = Layer.RaycastEndStop;
+        layerChangeObservers(layerHit);
     }
 
     RaycastHit? RaycastForLayer(Layer layer)
--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -1,17 +1,46 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour {
 
 	[SerializeField] float maxHealthPoints = 100f;
+	[SerializeField] float attackRadius = 4f;
 
 	float currentHealthPoints = 100f;
+
+	float distanceToPlayer;
+	AICharacterControl aiCharicterControl = null;
+
+	GameObject player;
+
 	public float healthAsPercentage
 	{
 		get
 		{
 			return currentHealthPoints / (float)maxHealthPoints;
+		}
+	}
+
+	void Start()
+	{
+		player = GameObject.FindGameObjectWithTag("Player");
+		aiCharicterControl = GetComponent<AICharacterControl>();
+	}
+
+	void Update()
+	{
+		distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
+		//Debug.Log(distanceToPlayer);
+
+		if (distanceToPlayer <= attackRadius)
+		{
+			aiCharicterControl.SetTarget(player.transform);
+		}
+		else 
+		{
+			aiCharicterControl.SetTarget(transform);
 		}
 	}
 
