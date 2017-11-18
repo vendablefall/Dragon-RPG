a9dc44c0d3af682e2f5ccf4fc865ffd0a603a9b2
vendablefall <vendablefall@leet.cloud>
2017-11-18 16:01:25 +1000

    added attackRadius sightRadius and chaseStopRadius to enemy

--- Assets/Enemy/Enemy.cs
+++ Assets/Enemy/Enemy.cs
@@ -4,25 +4,19 @@ using UnityEngine;
 using UnityStandardAssets.Characters.ThirdPerson;
 
 public class Enemy : MonoBehaviour , IDamagable {
-
 	[SerializeField] float maxHealthPoints = 100f;
-	[SerializeField] float attackRadius = 4f;
-
+	[SerializeField] float attackRadius = 2f;
+	[SerializeField] float sightRadius = 4f;
+	[SerializeField] float chaseStopRadius = 6f;
 	float currentHealthPoints = 100f;
-
 	float distanceToPlayer;
 	AICharacterControl aiCharicterControl = null;
-
 	GameObject player;
-
-	public float healthAsPercentage
+	public float healthAsPercentage { get { return currentHealthPoints / (float)maxHealthPoints; } }
+	public void TakeDamage(float damage)
 	{
-		get
-		{
-			return currentHealthPoints / (float)maxHealthPoints;
-		}
+		currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
 	}
-
 	void Start()
 	{
 		player = GameObject.FindGameObjectWithTag("Player");
@@ -34,19 +28,43 @@ public class Enemy : MonoBehaviour , IDamagable {
 		distanceToPlayer = Vector3.Distance(player.transform.position, transform.position);
 		//Debug.Log(distanceToPlayer);
 
-		if (distanceToPlayer <= attackRadius)
-		{
-			aiCharicterControl.SetTarget(player.transform);
-		}
-		else 
+		if (aiCharicterControl.target == player.transform && distanceToPlayer >= chaseStopRadius)
 		{
 			aiCharicterControl.SetTarget(transform);
 		}
+
+
+		if (distanceToPlayer <= sightRadius)
+		{
+			aiCharicterControl.SetTarget(player.transform);
+		}
+		// else 
+		// {
+		// 	aiCharicterControl.SetTarget(transform);
+		// }
+
+		if (distanceToPlayer <= attackRadius)
+		{
+			print("enemy will attack, once you imlpement attacking.......");
+			//aiCharicterControl.SetTarget(player.transform);
+			// TODO spawn projectile
+		}
+
 	}
 
-	public void TakeDamage(float damage)
+	void OnDrawGizmos()
 	{
-		currentHealthPoints = Mathf.Clamp(currentHealthPoints - damage, 0f, maxHealthPoints);
+
+		Gizmos.color = Color.red;
+		Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+		Gizmos.color = Color.blue;
+		Gizmos.DrawWireSphere(transform.position, sightRadius);
+
+		Gizmos.color = Color.green;
+		Gizmos.DrawWireSphere(transform.position, chaseStopRadius);
+
+
 	}
 
 
